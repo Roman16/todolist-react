d46b971ba18cf9138eed869cc3400aa4619b5271
Roman16 <irok2012@yandex.ru>
2018-10-22 13:36:38 +0300

    only react

--- src/component/App.js
+++ src/component/App.js
@@ -8,6 +8,7 @@ const update = require('immutability-helper');
 
 class Task {
     constructor(title) {
+        this.id = new Date().getTime();
         this.title = title;
         this.done = false;
     }
@@ -15,52 +16,32 @@ class Task {
 
 class App extends Component {
     state = {
-        tasks: [
-            {
-                id: 1,
-                title: 'Task-1',
-                done: false
-            },
-            {
-                id: 2,
-                title: 'Task-2',
-                done: true
-            },
-            {
-                id: 3,
-                title: 'Task-3',
-                done: false
-            },
-            {
-                id: 4,
-                title: 'Task-4',
-                done: false
-            },
-            {
-                id: 5,
-                title: 'Task-55',
-                done: true
-            },
-        ],
+        tasks: [],
         newTask: ''
+    };
+
+    save = () => {
+        localStorage.setItem('todoList', JSON.stringify(this.state.tasks))
     };
 
     add = () => {
         let task = new Task(this.state.newTask);
-        this.setState({tasks: [...this.state.tasks, task], newTask: ''});
+        this.setState({
+                tasks: [...this.state.tasks, task],
+                newTask: ''
+            },
+            () => this.save());
     };
 
     finishedTask = id => {
-        console.log(id);
         let doneTusk = this.state.tasks.map((item, index) => {
             if (id === index) item.done = !item.done;
             return item;
         });
-
         this.setState({
-            tasks: doneTusk
-        });
-        console.log(this.state.tasks);
+                tasks: doneTusk
+            },
+            () => this.save());
     };
 
     updateTusk = (id, task) => {
@@ -68,32 +49,39 @@ class App extends Component {
             if (id === index) item.title = task;
             return item;
         });
-
         this.setState({
-            tasks: updateTusk
-        });
-        console.log(this.state.tasks);
+                tasks: updateTusk
+            },
+            () => this.save());
     };
 
     deleteTask = id => {
         let newList = this.state.tasks;
         newList.splice(id, 1);
         this.setState({
-            tasks: newList
-        });
+                tasks: newList
+            },
+            () => this.save());
     };
 
     moveCard = (dragIndex, hoverIndex) => {
         const dragCard = this.state.tasks[dragIndex];
-
         this.setState(
             update(this.state, {
                 tasks: {
                     $splice: [[dragIndex, 1], [hoverIndex, 0, dragCard]],
                 },
             }),
-        )
+            () => this.save());
     };
+
+    componentDidMount() {
+        let pastList = JSON.parse(localStorage.getItem('todoList')) || [];
+
+        this.setState({
+            tasks: pastList
+        })
+    }
 
     render() {
         return (
